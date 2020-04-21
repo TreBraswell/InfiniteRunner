104c2ed1399817464cff5b8d83d8df8bdfc6336f
TreBraswell <49354031+TreBraswell@users.noreply.github.com>
2020-04-21 16:50:35 -0700

    ball rolling

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,16 +8,46 @@ class Menu extends Phaser.Scene {
         this.load.image('yarn', './assets/yarn.png');
       }
       create() {
-        this.updates = 3;
+        //how fast our ball rotates
+        this.updates = .1;
         this.player = this.add.image(320, 240, 'yarn');
-          // display menu text
+        //keys
+        this.keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);       
 
+        this.holdleft = false;
       }
     update() {
-        this.rotate();
-        }
-    rotate()
+      //checks if its down make it so the hold is true
+      if(Phaser.Input.Keyboard.JustDown(this.keyLEFT)|| this.holdleft)
+      {
+        this.rotateleft();
+        this.holdleft = true;
+      }
+      //if its up it is no longer holding
+      if(Phaser.Input.Keyboard.JustUp(this.keyLEFT))
+      {
+        this.holdleft = false;
+      }
+    //checks if its down make it so the hold is true
+      if(Phaser.Input.Keyboard.JustDown(this.keyRIGHT)||this.holdright)
+      {
+        this.rotateright();
+        this.holdright = true;
+      }
+      //checks if its down make it so the hold is true
+      if(Phaser.Input.Keyboard.JustUp(this.keyRIGHT))
+      {
+        this.holdright = false;
+      }
+
+   }
+    rotateright()
     {
         this.player.rotation +=  this.updates;
+    }
+    rotateleft()
+    {
+        this.player.rotation -=  this.updates;
     }
   }

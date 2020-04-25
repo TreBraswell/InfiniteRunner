18f4f2e996b3e20d9e6064361ace50282e3e14a7
TreBraswell <49354031+TreBraswell@users.noreply.github.com>
2020-04-25 12:09:00 -0700

    player prefab

---
+++ src/prefabs/Player.js
@@ -0,0 +1,55 @@
+// Player prefab
+class Player extends Phaser.Physics.Arcade.Sprite {
+    
+    constructor(scene, x,y,plat,cursor) {
+        // call Phaser Physics Sprite constructor
+        super(scene,x, y, plat); 
+        // set up physics sprite
+        scene.add.existing(this);               // add to existing scene, displayList, updateList
+        scene.physics.add.existing(this);       // add physics body
+        this.updates = .1;
+        this.jumps = 1;
+        this.jumping = false;
+        this.speed = 200;
+        this.JUMP_VELOCITY = -700;
+        this.scene = scene;
+        this.setCollideWorldBounds(true);
+        this.cursors = cursor;
+
+    }
+
+    update() {
+        if(this.cursors.left.isDown) {
+            this.rotation -=  this.updates;
+            this.body.setVelocityX(-this.speed);
+        } else if(this.cursors.right.isDown) {
+            this.body.setVelocityX(this.speed);
+            this.rotation +=  this.updates;
+        } else {
+            // set acceleration to 0 so DRAG will take over
+            this.body.setVelocityX(0);
+      
+        }
+    
+        // check if player is grounded
+        this.isGrounded = this.body.touching.down;
+        // if so, we have jumps to spare 
+        if(this.isGrounded) {
+           this.jumps = 1;
+          this.jumping = false;
+        } 
+          // allow steady velocity change up to a certain key down duration
+          // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.DownDuration__anchor
+      if(this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(this.cursors.up, 150)) {
+          
+        this.body.velocity.y = this.JUMP_VELOCITY;
+          this.jumping = true;
+      }
+        // finally, letting go of the UP key subtracts a jump
+       if(this.jumping && Phaser.Input.Keyboard.UpDuration(this.cursors.up)) {
+          this.jumps--;
+          this.jumping = false;
+        }
+        
+    }
+}
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,63 +9,33 @@ class Menu extends Phaser.Scene {
       }
       create() {
         this.Platformspeed = 200;
-        this.jumps = 1;
-        this.jumping = false;
         this.ship02 = new Platform(this,0,'platimage');
         this.physics.world.gravity.y = 2600;
         this.ship02.x = 320;
         this.ship02.y = 290;
-        this.speed = 200;
-        this.JUMP_VELOCITY = -700;
-        //how fast our ball rotates
-        this.updates = .1;
-        this.player = this.physics.add.image(320, 240, 'yarn');
-        this.player.setCollideWorldBounds(true);  
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.physics.add.collider( this.player,this.ship02);
+        this.playerGroup = this.add.group({
+          runChildUpdate: true    // make sure update runs on group children
+      });
+
       this.platformGroup = this.add.group({
           runChildUpdate: true    // make sure update runs on group children
       });
+      this.platformGroup.add(this.ship02);
+      this.addPlayer();
       this.addPlatform();
-      this.physics.add.collider( this.player,this.platformGroup);
+      this.physics.add.collider( this.platformGroup,this.playerGroup);
       }
       addPlatform() {
         let plat = new Platform(this, this.Platformspeed,'platimage');     // create new barrier
         this.platformGroup.add(plat);                         // add it to existing group
     }
+    addPlayer(){
+      let player = new Player(this,320, 240, 'yarn',this.input.keyboard.createCursorKeys());
+      this.playerGroup.add(player);  
+    }
     update() {
-      //checks if its down make it so the hold is true
-      if(this.cursors.left.isDown) {
-        this.player.rotation -=  this.updates;
-        this.player.body.setVelocityX(-this.speed);
-    } else if(this.cursors.right.isDown) {
-        this.player.body.setVelocityX(this.speed);
-        this.player.rotation +=  this.updates;
-    } else {
-        // set acceleration to 0 so DRAG will take over
-        this.player.body.setVelocityX(0);
-  
-    }
-
-    // check if player is grounded
-    this.player.isGrounded = this.player.body.touching.down;
-    // if so, we have jumps to spare 
-    if(this.player.isGrounded) {
-       this.jumps = 1;
-      this.jumping = false;
-    } 
-      // allow steady velocity change up to a certain key down duration
-      // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.DownDuration__anchor
-  if(this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(this.cursors.up, 150)) {
       
-    this.player.body.velocity.y = this.JUMP_VELOCITY;
-      this.jumping = true;
-  }
-    // finally, letting go of the UP key subtracts a jump
-   if(this.jumping && Phaser.Input.Keyboard.UpDuration(this.cursors.up)) {
-      this.jumps--;
-      this.jumping = false;
-    }
+     
 
    }
 
